76fc024be0e4634fae757efd1f16b30dfab447fe
Matheus Soares <matheussoaresmjs@gmail.com>
2023-05-09 20:46:11 -0300

    alterei rashCode

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dslist.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -120,11 +122,10 @@ public class Game {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+        return Objects.hash(id);
     }
+
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -141,6 +142,5 @@ public class Game {
             return false;
         return true;
     }
-
     
 }
